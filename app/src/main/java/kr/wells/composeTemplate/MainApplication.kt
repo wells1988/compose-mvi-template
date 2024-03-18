d6a0a619465d1ff7ed6185f43083dd09f3a0089a
wells1988 <eni.wells@gmail.com>
2024-03-18 23:09:54 +0900

    add data, domain module

---
+++ app/src/main/java/kr/wells/composeTemplate/MainApplication.kt
@@ -0,0 +1,20 @@
+package kr.wells.composeTemplate
+
+import android.app.Application
+import org.koin.android.BuildConfig
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class MainApplication:Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
+            androidContext(this@MainApplication)
+            modules(appModule)
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "compose-mvi-template"
 include(":app")
- 
+include(":data")
+include(":domain")
